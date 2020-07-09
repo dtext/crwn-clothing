3b1460b20edb6076769d958223a05f8cc6bef4aa
Danny Textores <textores.danny@gmail.com>
2020-07-09 17:46:54 +0200

    remove item from cart on click of x

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,16 +2,23 @@ import React from "react";
 
 import "./checkout-item.styles.scss"
 
-const CheckoutItem = ({item: {imageUrl, name, price, quantity}}) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img alt='item' src={imageUrl}/>
-    </div>
-    <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
-    <span className='price'>{price}€</span>
-    <div className='remove'>&#10005;</div>
-  </div>
-)
+import {connect} from "react-redux"
 
-export default CheckoutItem
+import {clearItemFromCart} from "../../redux/cart/cart.actions";
+
+const CheckoutItem = ({item, dispatch}) => {
+  const {imageUrl, name, price, quantity} = item
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img alt='item' src={imageUrl}/>
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>{quantity}</span>
+      <span className='price'>{price}€</span>
+      <div className='remove-button' onClick={() => dispatch(clearItemFromCart(item))}>&#10005;</div>
+    </div>
+  )
+}
+
+export default connect(null)(CheckoutItem)
