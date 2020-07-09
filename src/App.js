d4ae89e597e24b17f59b60f93025087bb4753fef
Danny Textores <textores.danny@gmail.com>
2020-07-09 13:57:59 +0200

    implement redirect from auth page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
-import { connect } from 'react-redux'
-import { setCurrentUser} from "./redux/user/user.actions";
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {setCurrentUser} from "./redux/user/user.actions";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import AuthPage from "./pages/auth/auth.component";
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
+import SignIn from "./components/sign-in/sign-in.component";
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
+    const {setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
@@ -42,15 +43,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={AuthPage}/>
+          <Route exact path='/signin' render={() =>
+            this.props.currentUser ? (
+              <Redirect to='/'/>
+            ) : (
+              <AuthPage/>
+            )
+          }/>
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
