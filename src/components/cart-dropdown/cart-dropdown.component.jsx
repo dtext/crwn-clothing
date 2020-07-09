c7d7143ec82c29b560b89058ad02c2c74a749820
Danny Textores <textores.danny@gmail.com>
2020-07-09 16:37:00 +0200

    display message on emtpy cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,7 +12,9 @@ const CartDropdown = ({items}) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
-        items.map(item => <CartItem id={item.id} item={item}/>)
+        items.length < 1
+          ? <span className='empty-message'>no items in cart</span>
+          : items.map(item => <CartItem id={item.id} item={item}/>)
       }
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
