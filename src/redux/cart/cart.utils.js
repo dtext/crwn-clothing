6666803a1e606839e4301405623b78f69a95ed79
Danny Textores <textores.danny@gmail.com>
2020-07-10 10:11:15 +0200

    implement remove item action to manipulate item quantity in cart on checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,27 @@ export const addItemToCart = (cartItems, item) => {
 
   return [...cartItems, {...item, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, item) => {
+  const existing = cartItems
+    .find(cartItem => cartItem.id === item.id)
+
+
+  if (existing && existing.quantity === 1) {
+    // item found and only one of that type in cart:
+    // remove from cart
+    return cartItems.filter(cartItem => cartItem.id !== item.id)
+  } else if (existing) {
+    // item found and more of that type in cart:
+    // decrease quantity
+    return cartItems
+      .map(cartItem => (
+        cartItem.id === item.id
+          ? {...cartItem, quantity: cartItem.quantity - 1}
+          : cartItem
+      ))
+  } else {
+    // item not found: do nothing
+    return cartItems
+  }
+}
