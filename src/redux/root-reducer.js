e2e6926bcf42bd6022f1a3330422084726437443
Danny Textores <textores.danny@gmail.com>
2020-07-10 11:02:45 +0200

    use redux for shop collections

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from "redux-persist/lib/storage"
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: 'root',
@@ -18,5 +19,6 @@ export default persistReducer(
     user: userReducer,
     cart: cartReducer,
     directory: directoryReducer,
+    shop: shopReducer,
   })
 )
