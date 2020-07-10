270b83283f48397d1164280e862f6d85a013bdb1
Danny Textores <textores.danny@gmail.com>
2020-07-10 10:11:39 +0200

    add functionality to manipulate quantity on checkout

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,9 +4,9 @@ import "./checkout-item.styles.scss"
 
 import {connect} from "react-redux"
 
-import {clearItemFromCart} from "../../redux/cart/cart.actions";
+import {clearItemFromCart, addItem, removeItem} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({item, dispatch}) => {
+const CheckoutItem = ({item, clearItem, addItem, removeItem}) => {
   const {imageUrl, name, price, quantity} = item
   return (
     <div className='checkout-item'>
@@ -14,11 +14,21 @@ const CheckoutItem = ({item, dispatch}) => {
         <img alt='item' src={imageUrl}/>
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => removeItem(item)}>&#9667;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(item)}>&#9657;</div>
+      </span>
       <span className='price'>{price}€</span>
-      <div className='remove-button' onClick={() => dispatch(clearItemFromCart(item))}>&#10005;</div>
+      <div className='remove-button' onClick={() => clearItem(item)}>&#10005;</div>
     </div>
   )
 }
 
-export default connect(null)(CheckoutItem)
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
